820752b28889166547b9b9e346f4a29232a6f8fe
trestletech <jeff.allen@trestletechnology.net>
2018-03-07 11:46:24 -0600

    Validate all deps JSON files.

--- validate.sh
+++ validate.sh
@@ -1,13 +1,20 @@
+#!/bin/bash
+
 set -e
 
 ./node_modules/.bin/ajv compile -s schema.json
-if ! diff -B -q schema.json <(./node_modules/.bin/jsonlint schema.json); then
+if ! diff -q schema.json <(./node_modules/.bin/jsonlint schema.json); then
   echo "JSON lint errors in schema.json. Run 'npm run lint' to clean up."
-  diff -B schema.json <(./node_modules/.bin/jsonlint schema.json)
+  diff schema.json <(./node_modules/.bin/jsonlint schema.json)
   exit 1
 fi
 
 for file in deps/*
 do 
   ./node_modules/.bin/ajv validate -s schema.json -d $file
+  if ! diff -q $file <(./node_modules/.bin/jsonlint $file); then
+    echo "JSON lint errors in $file. Run 'npm run lint' to clean up."
+    diff $file <(./node_modules/.bin/jsonlint $file)
+    exit 1
+  fi
 done
