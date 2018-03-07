57170598d807e3d6b31246b4a1a2715cd7bc596d
trestletech <jeff.allen@trestletechnology.net>
2018-03-07 11:43:08 -0600

    Support validating JSON files for lint.

--- validate.sh
+++ validate.sh
@@ -1,7 +1,11 @@
 set -e
 
 ./node_modules/.bin/ajv compile -s schema.json
-./node_modules/.bin/jsonlint schema.json
+if ! diff -B -q schema.json <(./node_modules/.bin/jsonlint schema.json); then
+  echo "JSON lint errors in schema.json. Run 'npm run lint' to clean up."
+  diff -B schema.json <(./node_modules/.bin/jsonlint schema.json)
+  exit 1
+fi
 
 for file in deps/*
 do 
